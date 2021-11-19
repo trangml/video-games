bd44744a6dec13875ed21c1ec178caa6d3f08ecc
trangml <mattluutrang@vt.edu>
2021-11-19 17:09:14 -0500

    a

--- hw10_coffee_table/assets/cuboid.js
+++ hw10_coffee_table/assets/cuboid.js
@@ -1,7 +1,64 @@
+/**
+ * This file contains the implementation of the Cuboid class, the BigCuboidClass, and the Face class.
+ */
 var nodeSize = 8;
 
-var createCuboid = function (x, y, z, w, h, d) {
-    var nodes = [[x, y, z],
+class Face {
+  /**
+   * The Face class contains the drawing logic for a singular face of a cubiod, as well as the centroid
+   */
+  constructor(points, color, normZ) {
+    this.points = points;
+    this.color = color;
+    this.z = normZ;
+  }
+  getCentroid() {
+    /**
+     * Returns the combined z value of the points in this face
+     */
+    var z = 0;
+    for (var i = 0; i < this.points.length; i++) {
+      z += this.points[i][2];
+    }
+    return z;
+  }
+  draw() {
+    /**
+     * Draws the face
+     */
+    if (this.color !== "none") {
+      //strokeWeight(1);
+      stroke(this.color);
+      noStroke();
+      fill(this.color);
+      beginShape();
+      for (var i = 0; i < this.points.length; i++) {
+        vertex(this.points[i][0], this.points[i][1]);
+      }
+      endShape(CLOSE);
+    }
+  }
+}
+
+class Cuboid {
+  /**
+   * The Cuboid contains the logic for drawing a cuboid, as well as the centroid. It has the
+   * necessary nodes, edges, normal vectors, and faces.
+   */
+  constructor(x, y, z, w, h, d, colors = ["red", "green", "yellow", "blue", "purple", "orange"]) {
+    if (w < 0) {
+      x += w;
+      w *= -1;
+    }
+    if (h < 0) {
+      y += h;
+      h *= -1;
+    }
+    if (d < 0) {
+      z += d;
+      d *= -1;
+    }
+    this.nodes = [[x, y, z],
     [x, y, z + d],
     [x, y + h, z],
     [x, y + h, z + d],
@@ -9,75 +66,252 @@ var createCuboid = function (x, y, z, w, h, d) {
     [x + w, y, z + d],
     [x + w, y + h, z],
     [x + w, y + h, z + d]];
-    var edges = [[0, 1], [1, 3], [3, 2], [2, 0],
+    this.edges = [[0, 1], [1, 3], [3, 2], [2, 0],
     [4, 5], [5, 7], [7, 6], [6, 4],
     [0, 4], [1, 5], [2, 6], [3, 7]];
-    var norm = [[x, y + h / 2, z + d / 2],
+    this.norm = [[x, y + h / 2, z + d / 2],
     [x + w, y + h / 2, z + d / 2],
     [x + w / 2, y + h / 2, z],
     [x + w / 2, y + h / 2, z + d],
     [x + w / 2, y, z + d / 2],
     [x + w / 2, y + h, z + d / 2]];
-    var normB = [[x - w, y + h / 2, z + d / 2],
+    this.normB = [[x - w, y + h / 2, z + d / 2],
     [x + 2 * w, y + h / 2, z + d / 2],
     [x + w / 2, y + h / 2, z - d],
     [x + w / 2, y + h / 2, z + 2 * d],
     [x + w / 2, y - h, z + d / 2],
-    [x + w / 2, y + h, z + d / 2]];
-    var vecs = [norm, normB];
+    [x + w / 2, y + 2 * h, z + d / 2]];
+    this.vecs = [this.norm, this.normB];
 
     // left, right, back, front, bottom, top
-    var faces = [[0, 1, 3, 2],
+    this.faces = [[0, 1, 3, 2],
     [4, 5, 7, 6],
     [0, 2, 6, 4],
     [1, 3, 7, 5],
     [0, 1, 5, 4],
     [2, 3, 7, 6]];
 
-    var colors = ["red", "green", "yellow", "blue", "purple", "orange"]
+    this.colors = colors;
+  }
+  draw() {
+    /**
+     * Draws the cuboid wireframe and shown faces
+     */
+    var vecs, nodes, edges;
+    // Draw edges
+    stroke(edgeColour);
 
-    return { 'nodes': nodes, 'edges': edges };
-};
+    nodes = this.nodes;
+    edges = this.edges;
+    vecs = this.vecs;
 
-var shape1, shape2, shape3;
-var shapes = [];
+    for (var e = 0; e < edges.length; e++) {
+      var n0 = edges[e][0];
+      var n1 = edges[e][1];
+      var node0 = nodes[n0];
+      var node1 = nodes[n1];
+      line(node0[0], node0[1], node1[0], node1[1]);
+    }
 
-// Rotate shape around the z-axis
-var rotateZ3D = function (theta, nodes) {
+    // Draw nodes
+    fill(nodeColour);
+    noStroke();
+    nodes = this.nodes;
+
+    for (var n = 0; n < nodes.length; n++) {
+      var node = nodes[n];
+      ellipse(node[0], node[1], nodeSize, nodeSize);
+    }
+    // Draw faces
+    for (var i = 0; i < vecs[0].length; i++) {
+      fill(this.colors[i]);
+      var normZ = - vecs[1][i][2] + vecs[0][i][2];
+      if (normZ > 0) {
+        var nodeIds = this.faces[i];
+        beginShape();
+        vertex(nodes[nodeIds[0]][0], nodes[nodeIds[0]][1]);
+        vertex(nodes[nodeIds[1]][0], nodes[nodeIds[1]][1]);
+        vertex(nodes[nodeIds[2]][0], nodes[nodeIds[2]][1]);
+        vertex(nodes[nodeIds[3]][0], nodes[nodeIds[3]][1]);
+        endShape(CLOSE);
+      }
+    }
+  }
+
+  getDrawnFaces() {
+    /**
+     * Returns the faces of the cuboid that are drawn
+     */
+    var vecs, nodes, edges;
+    nodes = this.nodes;
+    edges = this.edges;
+    vecs = this.vecs;
+    var faces = [];
+    for (var i = 0; i < vecs[0].length; i++) {
+      var normZ = -vecs[1][i][2] + vecs[0][i][2];
+      if (normZ > -3) {
+        var nodeIds = this.faces[i];
+        var shape = [nodes[nodeIds[0]], nodes[nodeIds[1]], nodes[nodeIds[2]], nodes[nodeIds[3]]];
+        faces.push(new Face(shape, this.colors[i], normZ));
+      }
+    }
+    return faces;
+  }
+  rotateZ3D(theta, nodes = this.nodes, vecs = this.vecs) {
+    /**
+     * Rotates the cuboid around the z axis
+     */
     var sinTheta = sin(theta);
     var cosTheta = cos(theta);
 
     for (var n = 0; n < nodes.length; n++) {
-        var node = nodes[n];
-        var x = node[0];
-        var y = node[1];
-        node[0] = x * cosTheta - y * sinTheta;
-        node[1] = y * cosTheta + x * sinTheta;
+      var node = nodes[n];
+      var x = node[0];
+      var y = node[1];
+      node[0] = x * cosTheta - y * sinTheta;
+      node[1] = y * cosTheta + x * sinTheta;
     }
-};
+    for (var n = 0; n < vecs[0].length; n++) {
+      var node1 = vecs[0][n];
+      var node2 = vecs[1][n];
+      var x = node1[0];
+      var y = node1[1];
+      var xb = node2[0];
+      var yb = node2[1];
+      node1[0] = x * cosTheta - y * sinTheta;
+      node1[1] = y * cosTheta + x * sinTheta;
+      node2[0] = xb * cosTheta - yb * sinTheta;
+      node2[1] = yb * cosTheta + xb * sinTheta;
+    }
+  };
 
-var rotateY3D = function (theta, nodes) {
+  rotateY3D(theta, nodes = this.nodes, vecs = this.vecs) {
+    /**
+     * Rotates the cuboid around the y axis
+     */
     var sinTheta = sin(theta);
     var cosTheta = cos(theta);
 
     for (var n = 0; n < nodes.length; n++) {
-        var node = nodes[n];
-        var x = node[0];
-        var z = node[2];
-        node[0] = x * cosTheta - z * sinTheta;
-        node[2] = z * cosTheta + x * sinTheta;
+      var node = nodes[n];
+      var x = node[0];
+      var z = node[2];
+      node[0] = x * cosTheta - z * sinTheta;
+      node[2] = z * cosTheta + x * sinTheta;
     }
-};
+    for (var n = 0; n < vecs[0].length; n++) {
+      var node1 = vecs[0][n];
+      var node2 = vecs[1][n];
+      var x = node1[0];
+      var z = node1[2];
+      var xb = node2[0];
+      var zb = node2[2];
+      node1[0] = x * cosTheta - z * sinTheta;
+      node1[2] = z * cosTheta + x * sinTheta;
+      node2[0] = xb * cosTheta - zb * sinTheta;
+      node2[2] = zb * cosTheta + xb * sinTheta;
+    }
+  };
 
-var rotateX3D = function (theta, nodes) {
+  rotateX3D(theta, nodes = this.nodes, vecs = this.vecs) {
+    /**
+     * Rotates the cuboid around the x axis
+     */
     var sinTheta = sin(theta);
     var cosTheta = cos(theta);
 
     for (var n = 0; n < nodes.length; n++) {
-        var node = nodes[n];
-        var y = node[1];
-        var z = node[2];
-        node[1] = y * cosTheta - z * sinTheta;
-        node[2] = z * cosTheta + y * sinTheta;
+      var node = nodes[n];
+      var y = node[1];
+      var z = node[2];
+      node[1] = y * cosTheta - z * sinTheta;
+      node[2] = z * cosTheta + y * sinTheta;
     }
+    for (var n = 0; n < vecs[0].length; n++) {
+      var node1 = vecs[0][n];
+      var node2 = vecs[1][n];
+      var z = node1[2];
+      var y = node1[1];
+      var zb = node2[2];
+      var yb = node2[1];
+      node1[1] = y * cosTheta - z * sinTheta;
+      node1[2] = z * cosTheta + y * sinTheta;
+      node2[1] = yb * cosTheta - zb * sinTheta;
+      node2[2] = zb * cosTheta + yb * sinTheta;
+    }
+  };
 };
+
+class BigCuboid {
+  /**
+   * The BigCuboid class contains a larger cuboid which is then broken down into smaller cuboids
+   */
+  constructor(x, y, z, w, h, d, colors = ["red", "green", "yellow", "blue", "purple", "orange"]) {
+    if (w < 0) {
+      x += w;
+      w *= -1;
+    }
+    if (h < 0) {
+      y += h;
+      h *= -1;
+    }
+    if (d < 0) {
+      z += d;
+      d *= -1;
+    }
+    this.cuboids = [];
+    this.scale = 1;
+    this.cubeSizeX = w / this.scale;
+    this.cubeSizeY = h / this.scale;
+    this.cubeSizeZ = d / this.scale;
+    this.size = 10
+    this.cubeSizeX = this.size;
+    this.cubeSizeY = this.size;
+    this.cubeSizeZ = this.size;
+    var hideOverlap = 2;
+    // calculate the visible borders
+    var x_bounds = [0, w - this.cubeSizeX]
+    var y_bounds = [0, h - this.cubeSizeY]
+    var z_bounds = [0, d - this.cubeSizeZ]
+    for (var i = 0; i < w; i += this.cubeSizeX) {
+      for (var j = 0; j < h; j += this.cubeSizeY) {
+        for (var k = 0; k < d; k += this.cubeSizeZ) {
+          // if we are on a visible border
+          if (x_bounds.indexOf(i) !== -1 || y_bounds.indexOf(j) !== -1 || z_bounds.indexOf(k) !== -1)
+            // push a cuboid
+            this.cuboids.push(new Cuboid(x + i - hideOverlap, y + j - hideOverlap, z + k - hideOverlap, this.cubeSizeX + hideOverlap, this.cubeSizeY + hideOverlap, this.cubeSizeZ + hideOverlap, colors));
+
+        }
+      }
+    }
+  }
+  getDrawnFaces() {
+    /**
+     * Get the drawn faces for each cuboid
+     */
+    var faces = [];
+    for (var i = 0; i < this.cuboids.length; i++) {
+      faces = faces.concat(this.cuboids[i].getDrawnFaces());
+    }
+    return faces;
+  }
+  // Rotate shape around the z-axis
+  rotateZ3D(theta) {
+    for (var i = 0; i < this.cuboids.length; i++) {
+      this.cuboids[i].rotateZ3D(theta);
+    }
+  };
+  // Rotate shape around the y-axis
+  rotateY3D(theta) {
+    for (var i = 0; i < this.cuboids.length; i++) {
+      this.cuboids[i].rotateY3D(theta);
+    }
+  };
+  // Rotate shape around the x-axis
+  rotateX3D(theta) {
+    for (var i = 0; i < this.cuboids.length; i++) {
+      this.cuboids[i].rotateX3D(theta);
+    }
+  };
+
+}
--- hw10_coffee_table/sketch.js
+++ hw10_coffee_table/sketch.js
@@ -6,74 +6,119 @@
  *
  *
  * This assignment is a 3D coffee table game. The coffee table is modeled in 3D using only a
- * wireframe. It can be rotated using the mouse.
+ * 2D wireframe. It can be rotated using the mouse to change the perspective. The table is made of
+ * multiple colors.
  */
 
 
 // Declare variables for the components
-
+var shape1, shape2, shape3;
+var shapes = [];
+var browns = [];
 var backgroundColour, nodeColour, edgeColour;
 
 function setup() {
+  /**
+   * Setup the canvas and other variables
+   */
   createCanvas(400, 400);
   angleMode(DEGREES);
+
+  // Set up list of colors
+  browns = ["#3e2917", "#784d27", "#5b2f08", "#884c18", "#503822", "#623c1b", "#5d2e04"]
   backgroundColour = color(255, 255, 255);
   nodeColour = color(40, 168, 107);
   edgeColour = color(34, 68, 204);
 
-  shape1 = createCuboid(-120, -20, -20, 240, 40, 40);
-  shape2 = createCuboid(-120, -50, -30, -20, 100, 60);
-  shape3 = createCuboid(120, -50, -30, 20, 100, 60);
-  shapes = [shape1, shape2, shape3];
+  // create the colors for the tabletop
+  var tableColors1 = [browns[0], browns[0], browns[0], browns[0], browns[1], browns[1]];
+  var tableColors2 = [browns[0], browns[0], browns[0], browns[0], browns[2], browns[2]];
+  // Start with the tabletop, push all the shapes
+  shapes.push(new BigCuboid(-140, -50, -70, 70, 10, 70, colors = tableColors1),
+    new BigCuboid(-140, -50, 0, 70, 10, 70, colors = tableColors2),
+    new BigCuboid(-70, -50, -70, 70, 10, 70, colors = tableColors2),
+    new BigCuboid(-70, -50, 0, 70, 10, 70, colors = tableColors1),
+    new BigCuboid(0, -50, -70, 70, 10, 70, colors = tableColors1),
+    new BigCuboid(0, -50, 0, 70, 10, 70, colors = tableColors2),
+    new BigCuboid(70, -50, -70, 70, 10, 70, colors = tableColors2),
+    new BigCuboid(70, -50, 0, 70, 10, 70, colors = tableColors1),
+  )
 
-  //rotateZ3D(30);
-  //rotateY3D(30);
-  //rotateX3D(30);
+  // Create the colors for the legs
+  var legs1 = [browns[4], browns[0], browns[5], browns[0], "none", browns[3]];
+  var legs2 = [browns[4], browns[0], browns[0], browns[5], "none", browns[3]];
+  var legs3 = [browns[0], browns[4], browns[0], browns[5], "none", browns[3]];
+  var legs4 = [browns[0], browns[4], browns[5], browns[0], "none", browns[3]];
+  // Add the legs
+  shapes.push(new BigCuboid(-140, -40, -70, 20, 100, 20, colors = legs1),
+    new BigCuboid(-140, -40, 70, 20, 100, -20, colors = legs2),
+    new BigCuboid(140, -40, 70, -20, 100, -20, colors = legs3),
+    new BigCuboid(140, -40, -70, -20, 100, 20, colors = legs4),
+  )
+
+  // add the inner walls
+  var wall1 = [browns[3], browns[3], browns[3], browns[3], "none", browns[6]];
+  shapes.push(new BigCuboid(-120, -40, -60, 2 * 120, 10, 10, colors = wall1),
+    new BigCuboid(-120, -40, 60, 2 * 120, 10, -10, colors = wall1),
+    new BigCuboid(130, -40, -50, -10, 10, 100, colors = wall1),
+    new BigCuboid(-130, -40, -50, 10, 10, 100, colors = wall1),
+  )
+  // add the lower shelf
+  var shelfColors = [browns[0], browns[0], browns[0], browns[0], browns[1], browns[6]];
+  shapes.push(new BigCuboid(-120, 20, -50, 2 * 120, 10, 100, colors = shelfColors))
 }
 
-var draw = function () {
+function drawBackground() {
+  /**
+   * Draw the background grid
+   */
   background(backgroundColour);
-  var nodes, edges;
-
-  push();
-  translate(200, 200);
-  // Draw edges
-  stroke(edgeColour);
-
-  for (var shapeNum = 0; shapeNum < shapes.length; shapeNum++) {
-    nodes = shapes[shapeNum].nodes;
-    edges = shapes[shapeNum].edges;
-
-    for (var e = 0; e < edges.length; e++) {
-      var n0 = edges[e][0];
-      var n1 = edges[e][1];
-      var node0 = nodes[n0];
-      var node1 = nodes[n1];
-      line(node0[0], node0[1], node1[0], node1[1]);
+  fill("lightgrey")
+  noStroke();
+  for (var i = 0; i < 20; i++) {
+    var count = i % 2;
+    for (var j = 0; j < 20; j++) {
+      if (count % 2 == 0) {
+        rect(i * 20, j * 20, 20, 20);
+      }
+      count++;
     }
   }
+}
+var draw = function () {
+  /**
+   * Draws the screen
+   */
+  drawBackground();
+  // move to the center of the screen
+  push();
+  translate(200, 200);
+  var faces = [];
+  // Get the list of all the faces that are drawn
+  for (var i = 0; i < shapes.length; i++) {
+    faces = faces.concat(shapes[i].getDrawnFaces());
+  }
+  // Sort the faces by their average z centroid, furthest to closest
+  faces = faces.sort(function (a, b) {
+    if (a.getCentroid() < b.getCentroid()) return 1; else return -1;
+  });
 
-  // Draw nodes
-  fill(nodeColour);
-  noStroke();
-  for (var shapeNum = 0; shapeNum < shapes.length; shapeNum++) {
-    nodes = shapes[shapeNum].nodes;
-
-    for (var n = 0; n < nodes.length; n++) {
-      var node = nodes[n];
-      ellipse(node[0], node[1], nodeSize, nodeSize);
-    }
+  // Draw each face in order
+  for (var i = 0; i < faces.length; i++) {
+    faces[i].draw();
   }
   pop();
 };
 
 mouseDragged = function () {
+  /**
+   * If the mouse is dragged, rotate the table
+   */
   var dx = mouseX - pmouseX;
   var dy = mouseY - pmouseY;
 
   for (var shapeNum = 0; shapeNum < shapes.length; shapeNum++) {
-    var nodes = shapes[shapeNum].nodes;
-    rotateY3D(dx, nodes);
-    rotateX3D(dy, nodes);
+    shapes[shapeNum].rotateX3D(dy);
+    shapes[shapeNum].rotateY3D(dx);
   }
 };
